9ee4893f530fc928892603ec8fbcee029a77aec2
‪zeyad ashraf‬‏ <84470860+zeyadashraf2002@users.noreply.github.com>
2026-01-14 23:44:11 +0200

    Change environment variable for server port

--- storage-server.js
+++ storage-server.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.STORAGE_PORT || 5001;
+const PORT = process.env.PORT || 5001;
 const API_KEY = process.env.STORAGE_SERVER_API_KEY;
 
 if (!API_KEY) {
